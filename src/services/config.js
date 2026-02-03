c0f5cd51efef42099bec0c1e092377c8f7b72e02
Govind <govindju@gmail.com>
2026-02-03 17:15:12 +0530

    react client connected with deployed backend

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 const config = {
     BASE_URL: 'http://localhost:8080',
-    ADMIN_API: 'http://localhost:8080/api/admin',
+    ADMIN_API: 'https://foodgram-spring-backend-for-delivery-person-production.up.railway.app/api/admin',
     RESTAURANT_API: 'http://localhost:8080/api/admin/restaurants'
 }
 export default config
